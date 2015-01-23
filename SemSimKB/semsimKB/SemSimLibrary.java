4ccf4789f6e55b055e81a9bbd867a9a7070c903d
thompsct <thompsct54701@gmail.com>
2015-01-23 11:19:48 -0600

    Brought in the D3 visualization resources from SemGen.

---
+++ SemSimKB/semsimKB/SemSimLibrary.java
@@ -0,0 +1,178 @@
+package semsimKB;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLException;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+import semsim.model.physical.PhysicalEntity;
+import semsim.model.physical.PhysicalProcess;
+import semsimKB.model.annotation.ReferenceOntologyAnnotation;
+import semsimKB.model.physical.PhysicalProperty;
+import semsimKB.owl.SemSimOWLFactory;
+
+//Class for holding reference terms and data required for SemGen - intended to replace SemSimConstants class
+public class SemSimLibrary {
+	public static File ontologyTermsAndNamesCacheFile = new File("cfg/ontologyTermsAndNamesCache.txt");
+	private OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+	public OWLOntology OPB;
+	
+	private Hashtable<String, String[]> OPBClassesForUnitsTable = new Hashtable<String, String[]>();
+	private Hashtable<String, String[]> compositeAnnRelationsTableLR = new Hashtable<String, String[]>();
+	private Hashtable<String, String[]> compositeAnnRelationsTableRL = new Hashtable<String, String[]>();
+	private Hashtable<String, String[]> metadataRelationsTable = new Hashtable<String, String[]>();
+	private Hashtable<String, String[]> ontologyTermsAndNamesCache = new Hashtable<String,String[]>();
+	private Hashtable<String, String[]> jsimUnitsTable;
+	private Hashtable<String, String[]> jsimUnitPrefixesTable;
+	
+	// Hashtable that contains a hashtable of base units and exponents, and OPB classes
+	// Is creating a hashtable of hashtables frown upon?	
+	private static Set<String> OPBproperties = new HashSet<String>();
+	private static Set<String> OPBflowProperties = new HashSet<String>();
+	private static Set<String> OPBprocessProperties = new HashSet<String>();
+	private static Set<String> OPBdynamicalProperties = new HashSet<String>();
+	private static Set<String> OPBamountProperties = new HashSet<String>();
+	private static Set<String> OPBforceProperties = new HashSet<String>();
+	private static Set<String> OPBstateProperties = new HashSet<String>();
+	
+	public SemSimLibrary() {
+		loadLibrary();
+	}
+	
+	private void loadLibrary() {
+		try {
+			compositeAnnRelationsTableLR = ResourcesManager.createHashtableFromFile("cfg/structuralRelationsLR.txt");
+			compositeAnnRelationsTableRL = ResourcesManager.createHashtableFromFile("cfg/structuralRelationsRL.txt");
+			metadataRelationsTable = ResourcesManager.createHashtableFromFile("cfg/metadataRelations.txt");
+			ontologyTermsAndNamesCache = ResourcesManager.createHashtableFromFile("cfg/ontologyTermsAndNamesCache.txt");
+			jsimUnitsTable = ResourcesManager.createHashtableFromFile("cfg/jsimUnits");
+			jsimUnitPrefixesTable = ResourcesManager.createHashtableFromFile("cfg/jsimUnitPrefixes");
+			OPBClassesForUnitsTable = ResourcesManager.createHashtableFromFile("cfg/OPBClassesForUnits.txt");
+		} catch (FileNotFoundException e3) {e3.printStackTrace();}	
+		
+
+		// Load the local copy of the OPB and the SemSim base ontology, and other config files into memory
+		try {
+			OPB = manager.loadOntologyFromOntologyDocument(new File("cfg/OPB.970.owl"));
+			manager.loadOntologyFromOntologyDocument(new File("cfg/SemSimBase.owl"));
+		} catch (OWLOntologyCreationException e3) {
+			e3.printStackTrace();
+		}
+
+		try {
+			OPBproperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00147", false);
+			OPBflowProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00573", false);
+			OPBprocessProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB01151", false);
+			OPBdynamicalProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00568", false);
+			OPBamountProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00135", false);
+			OPBforceProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00574", false);
+			OPBstateProperties = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + "OPB_00569", false);
+		} catch (OWLException e2) {e2.printStackTrace();}
+	}
+	
+	public String[] getOPBUnitRefTerm(String unit) {
+		return OPBClassesForUnitsTable.get(unit);
+	}
+
+	public String[] getListofMetaDataRelations() {
+		return metadataRelationsTable.keySet().toArray(new String[]{});
+	}
+	
+	
+	public void storeCachedOntologyTerms(){
+		try {
+			PrintWriter writer = new PrintWriter(new FileWriter(ontologyTermsAndNamesCacheFile));
+			for(String key : ontologyTermsAndNamesCache.keySet()){
+				writer.println(key + "; " + ontologyTermsAndNamesCache.get(key)[0]);
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public OWLOntology getLoadedOntology(String onturi) throws OWLOntologyCreationException {
+		OWLOntology localont = SemSimOWLFactory.getOntologyIfPreviouslyLoaded(IRI.create(onturi), manager);
+		if (localont == null) {
+			localont = manager.loadOntologyFromOntologyDocument(IRI.create(onturi));
+		}
+		return localont;
+	}
+	
+	public Set<String> getOPBsubclasses(String parentclass) throws OWLException {
+		Set<String> subclassset = SemSimOWLFactory.getAllSubclasses(OPB, SemSimKBConstants.OPB_NAMESPACE + parentclass, false);
+		return subclassset;
+	}
+	
+	public OWLDataFactory makeOWLFactory() {
+		return manager.getOWLDataFactory() ;
+	}
+	
+	public boolean OPBhasProperty(String s) {
+		return OPBproperties.contains(s);
+	}
+	
+	public boolean OPBhasFlowProperty(ReferenceOntologyAnnotation roa) {
+		return OPBflowProperties.contains(roa.getReferenceURI().toString());
+	}
+	
+	public boolean OPBhasFlowProperty(String s) {
+		return OPBflowProperties.contains(s);
+	}
+	
+	public boolean OPBhasFlowProperty(URI u) {
+		return OPBflowProperties.contains(u.toString());
+	}
+	
+	public boolean OPBhasForceProperty(ReferenceOntologyAnnotation roa) {
+		return OPBforceProperties.contains(roa.getReferenceURI().toString());
+	}
+	
+	public boolean OPBhasAmountProperty(ReferenceOntologyAnnotation roa) {
+		return OPBamountProperties.contains(roa.getReferenceURI().toString());
+	}
+	
+	public boolean OPBhasStateProperty(ReferenceOntologyAnnotation roa) {
+		return OPBstateProperties.contains(roa.getReferenceURI().toString());
+	}
+	
+	public boolean OPBhasProcessProperty(ReferenceOntologyAnnotation roa) {
+		return OPBprocessProperties.contains(roa.getReferenceURI().toString());
+	}
+	
+	public boolean OPBhasProcessProperty(String s) {
+		return OPBprocessProperties.contains(s);
+	}
+	
+	public boolean OPBhasProcessProperty(URI u) {
+		return OPBprocessProperties.contains(u.toString());
+	}
+	
+	public boolean checkOPBpropertyValidity(PhysicalProperty prop, URI OPBuri){
+		if(prop.getPhysicalPropertyOf()!=null){
+			
+			// This conditional statement makes sure that physical processes are annotated with appropriate OPB terms
+			// It only limits physical entity properties to non-process properties. It does not limit based on whether
+			// the OPB term is for a constitutive property. Not sure if it should, yet.
+			if((prop.getPhysicalPropertyOf() instanceof PhysicalEntity || prop.getPhysicalPropertyOf() instanceof PhysicalProcess ) && 
+				(OPBhasFlowProperty(OPBuri) || OPBhasProcessProperty(OPBuri))) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
